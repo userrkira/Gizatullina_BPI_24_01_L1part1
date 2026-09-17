79ce1c62cefaa7339d21374667ddf4ee40f158d6
userr <userr@DESKTOP-4NL80KO>
2026-09-17 13:49:46 +0500

    Добавьте файлы проекта.

---
+++ Gizatullina_BPI_24_01_L1part1/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace Gizatullina_BPI_24_01_L1part1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string surname = SurnameTextBox.Text.Trim();
+
+                if (string.IsNullOrWhiteSpace(surname))
+                {
+                    throw new Exception("Введите фамилию.");
+                }
+
+                foreach (char symbol in surname)
+                {
+                    if (!char.IsLetter(symbol) && symbol != '-' && symbol != ' ')
+                    {
+                        throw new Exception("Фамилия должна содержать только буквы!!!");
+                    }
+                }
+
+                if (!decimal.TryParse(SalaryTextBox.Text, out decimal salary))
+                {
+                    throw new Exception("Оклад должен быть числом!!!");
+                }
+
+                if (salary <= 0)
+                {
+                    throw new Exception("Оклад должен быть больше нуля!!!");
+                }
+
+                if (!int.TryParse(YearTextBox.Text, out int year))
+                {
+                    throw new Exception("Год поступления должен быть целым числом!!!");
+                }
+
+                int currentYear = DateTime.Now.Year;
+
+                if (year < 1900 || year > currentYear)
+                {
+                    if (year < 0)
+                    {
+                        throw new Exception("Год не может быть отрицательным.");
+                    }
+                    throw new Exception(
+                        $"Год должен быть от 1900 до {currentYear}."
+                    );
+                }
+
+                // cоздаём объект работника
+                worker worker = new worker (
+                    surname,
+                    salary,
+                    year
+                );
+
+                int experience = worker.CalculateExperience();
+                int days = worker.CalculateDays();
+
+                ResultTextBlock.Text =
+                    $"Фамилия: {worker.Surname}\n" +
+                    $"Оклад($): {worker.Salary:N2} руб.\n" +
+                    $"Год поступления: {worker.YearOfEmployment}\n\n" +
+                    $"Стаж работы: {experience} лет\n" +
+                    $"Прошло дней: {days}";
+
+                ResultBorder.Visibility = Visibility.Visible;
+
+
+                SizeToContent = SizeToContent.Height;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    ex.Message,
+                    "Ошибка ввода",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                );
+            }
+        }
+    }
+}
---
+++ Gizatullina_BPI_24_01_L1part1/worker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gizatullina_BPI_24_01_L1part1
+{
+    class worker
+    {
+        public string Surname { get; set; }
+        public decimal Salary { get; set; }
+        public int YearOfEmployment { get; set; }
+
+        public worker(string surname, decimal salary, int yearOfEmployment)
+        {
+            Surname = surname;
+            Salary = salary;
+            YearOfEmployment = yearOfEmployment;
+        }
+
+
+        public int CalculateExperience()
+        {
+            return DateTime.Now.Year - YearOfEmployment;
+        }
+
+
+        public int CalculateDays()
+        {
+            DateTime startDate = new DateTime(YearOfEmployment, 1, 1);
+            return (DateTime.Now - startDate).Days;
+        }
+    }
+}
